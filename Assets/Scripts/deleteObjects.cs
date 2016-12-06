1d25fc3d729f10de264ec941712e09c890e85ea7
MikkiJordan <rasmus.j.v@hotmail.com>
2016-12-06 12:29:57 +0100

    Galaxy implementation and camera

--- Assets/Scripts/deleteObjects.cs
+++ Assets/Scripts/deleteObjects.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class deleteObjects : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    protected GameObject musicBlock;
+    protected GameObject[] musicBlocks;
+
+    // Use this for initialization
+    void Start()
+    {
+        musicBlock = GameObject.FindGameObjectWithTag("MusicBlock");
+        musicBlocks = GameObject.FindGameObjectsWithTag("MusicBlock");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == musicBlock)
+            Destroy(musicBlock);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            foreach (GameObject item in musicBlocks)
+                Destroy(item);
+    }
 }
